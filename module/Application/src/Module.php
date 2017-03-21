09ad103efb1241ab13ad89e02c744e53bf8b7874
ondrejd <ondrejd@gmail.com>
2017-03-21 23:21:50 +0100

    Added prototype for DataTable view helper.

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -9,24 +9,47 @@ namespace Application;
 
 use Application\Model\Contact;
 use Application\Model\ContactTable;
+use Application\Model\Destination;
+use Application\Model\DestinationTable;
 use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\TableGateway\TableGateway;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ViewHelperProviderInterface;
 use Zend\Mvc\MvcEvent;
 
-class Module
+/**
+ * Description of Module
+ *
+ * @author ondrejd
+ */
+class Module implements 
+    ConfigProviderInterface, 
+    ViewHelperProviderInterface
 {
+    /**
+     * @var string
+     */
     const VERSION = '3.0.2';
 
+    /**
+     * Returns module's configuration.
+     * @return array
+     */
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
     }
 
+    /**
+     * Returns configuration for service manager.
+     * @return array
+     */
     public function getServiceConfig()
     {
         return array(
             'factories' => array(
-                'Application\Model\ContactTable' =>  function($sm) {
+                ContactTable::class =>  function($sm) {
                     $tableGateway = $sm->get('ContactTableGateway');
                     $table = new ContactTable($tableGateway);
                     return $table;
@@ -34,18 +57,49 @@ class Module
                 'ContactTableGateway' => function($sm) {
                     $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
                     $resultSetPrototype = new ResultSet();
-                    $resultSetPrototype->setArrayObjectPrototype(new Album());
+                    $resultSetPrototype->setArrayObjectPrototype(new Contact());
                     return new TableGateway('contact', $dbAdapter, null, $resultSetPrototype);
+                },
+                DestinationTable::class =>  function($sm) {
+                    $tableGateway = $sm->get('DestinationTableGateway');
+                    $table = new DestinationTable($tableGateway);
+                    return $table;
+                },
+                'DestinationTableGateway' => function($sm) {
+                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Destination());
+                    return new TableGateway('destination', $dbAdapter, null, $resultSetPrototype);
                 },
             ),
         );
     }
 
+    /**
+     * Returns configuration for view helpers.
+     * @return array
+     */
+    public function getViewHelperConfig()
+    {
+        return [
+            'factories' => array(
+                'data_table' => function($sm) {
+                    $helper = new View\Helper\DataTable();
+                    return $helper;
+                }
+            ),
+        ];
+    }
+
+    /**
+     * On module bootstrap.
+     * @param MvcEvent $event
+     * @return void
+     */
     public function onBootstrap(MvcEvent $event)
     {
         //$serviceManager = $event->getApplication()->getServiceManager();
         $viewModel = $event->getApplication()->getMvcEvent()->getViewModel();
-
         $viewModel->language = 'cs';
     }
 }
